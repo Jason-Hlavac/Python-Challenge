ca2f881f833b7ddcb93d0264d5a31737488b748f
Jason-Hlavac <49419324+Jason-Hlavac@users.noreply.github.com>
2020-11-27 22:39:41 -0800

    Create Challenge2.py

---
+++ Challenge2.py
@@ -0,0 +1,10 @@
+message = "g fmnc wms bgblr rpylqjyrc gr zw fylb. rfyrq ufyr amknsrcpq ypc dmp. bmgle grgl zw fylb gq glcddgagclr ylb rfyr'q ufw rfgq rcvr gq qm jmle. sqgle qrpgle.kyicrpylq() gq pcamkkclbcb. lmu ynnjw ml rfc spj."
+url = "/pc/def/map"
+table = str.maketrans(
+    "abcdefghijklmnopqrstuvwxyz", "cdefghijklmnopqrstuvwxyzab"
+)
+
+result = message.translate(table)
+url = url.translate(table)
+print(result)
+print(url)
